e13b837fa428e55eaeeb10e32e8f6b5bcb56b1dd
Alec Higgins <100505370+alectries@users.noreply.github.com>
2026-02-25 15:43:06 -0500

    Add cluster maker and map generator functions

---
+++ R/make_clusters.R
@@ -0,0 +1,73 @@
+#' Randomly select census blocks and assign cluster identifiers
+#'
+#' Using U.S. Census data, randomly selects Census blocks, weighted by
+#' population, and assign numeric cluster identifiers to them.
+#'
+#' Unlike most clusteR functions, `make_clusters` can be run before `setup`
+#' because you need to identify clusters before you can identify households
+#' to survey.
+#'
+#' @param n The number of clusters to select.
+#' @param state The state where clusters will be selected. Defaults to the state in the config file; must be specified if running before `setup`.
+#' @param county The county where clusters will be selected. Defaults to the county in the config file; must be specified if running before `setup`.
+#' @importFrom cli style_bold
+#' @importFrom rlang abort
+#' @importFrom tibble tibble
+#' @importFrom tidycensus get_decennial
+#' @export
+
+make_clusters <- function(n,
+                          state = .cluster$cfg$state,
+                          county = .cluster$cfg$county
+){
+  # Check if state and county exist
+  if(is.null(state) && is.null(county)){
+    rlang::abort(message = c(
+      cli::style_bold("Geography not specified!"),
+      "i" = "Specify state and county."
+    ))
+  }
+
+  # If county is from config, modify it
+  if(exists(".cluster") && exists("cfg", where = .cluster) &&
+     exists("county", where = .cluster$cfg) && .cluster$cfg$county == county){
+    county <- substr(county, 3, 5)
+  }
+
+  # Get population data
+  dec <- tidycensus::get_decennial(
+    "block",
+    "P1_001N",
+    year = 2020,
+    state = state,
+    county = county
+  )
+
+  # Sample
+  geoid <- sample(dec$GEOID, size = n, prob = dec$value)
+
+  # Create geoids file
+  out <- tibble::tibble(
+    geoid = geoid,
+    cluster = 1:n
+  )
+  dir.create("Cohort", showWarnings = F)
+  readr::write_csv(out, "Cohort/geoids.csv")
+
+  # Save filepath to environment if it exists
+  if(exists(".cluster") && exists("cfg", where = .cluster)){
+    .cluster$cfg$geoids <- "Cohort/geoids.csv"
+    saveRDS(.cluster$cfg, "Scripts/config.rds")
+    rlang::inform(message = c(
+      cli::style_bold("Clusters saved."),
+      "v" = paste0("Saved to ", cli::style_underline("Cohort/geoids.csv"), "."),
+      "i" = "The geoids file has been saved to your clusteR configuration."
+    ))
+  } else {
+    rlang::inform(message = c(
+      cli::style_bold("Clusters saved."),
+      "v" = paste0("Saved to ", cli::style_underline("Cohort/geoids.csv"), "."),
+      "!" = "This clusteR environment is not configured. Manually add geoids in setup()."
+    ))
+  }
+}
---
+++ R/view_map.R
@@ -0,0 +1,88 @@
+#' Generate a map of clusters
+#'
+#' Produces a simple, labeled map of your clusters. Simpler than `make_groups`,
+#' which is for cluster grouping.
+#'
+#' @param title The title of the resulting map, if desired.
+#' @param subtitle The subtitle of the resulting map, if desired.
+#' @param fill The fill color for the clusters. Defaults to light gray.
+#' @importFrom dplyr filter
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 geom_sf
+#' @importFrom ggplot2 geom_sf_text
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_color_brewer
+#' @importFrom ggplot2 scale_fill_manual
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 theme_void
+#' @importFrom magrittr `%>%`
+#' @importFrom sf read_sf
+#' @export
+
+view_map <- function(title = NULL,
+                     subtitle = NULL,
+                     fill = "lightgray"
+){
+  # Definitions
+  `%>%` <- magrittr::`%>%`
+
+  # Get shapefiles
+  county <- sf::read_sf(.cluster$cfg$shape_county) %>%
+    dplyr::filter(GEOID == as.character(.cluster$cfg$county))
+  blocks <- sf::read_sf(.cluster$cfg$shape_block) %>%
+    dplyr::filter(
+      STATEFP20 == as.character(.cluster$cfg$state) &
+        COUNTYFP20 == substr(as.character(.cluster$cfg$county), 3, 5)
+    )
+
+  # Get geoids and merge
+  geoids <- readr::read_delim(.cluster$cfg$geoids, show_col_types = F)
+  clusters <- geoids %>%
+    dplyr::mutate(geoid = as.character(geoid)) %>%
+    dplyr::left_join(
+      dplyr::select(blocks, geoid = GEOID20, lat = INTPTLAT20, long = INTPTLON20,
+             ur = UR20, geometry),
+      by = "geoid"
+    )
+
+  # Map
+  plot_map <- ggplot2::ggplot() +
+    ggplot2::geom_sf(
+      ggplot2::aes(geometry = geometry),
+      data = county,
+      color = "lightblue4",
+      linewidth = 1,
+      fill = NA
+    ) +
+    ggplot2::geom_sf(
+      ggplot2::aes(geometry = geometry, color = ur),
+      data = clusters,
+      fill = fill,
+      linewidth = 1.05,
+      inherit.aes = F
+    ) +
+    ggplot2::geom_sf_text(
+      ggplot2::aes(label = cluster, geometry = geometry),
+      data = clusters,
+      size = 2.5
+    ) +
+    ggplot2::labs(
+      title = title,
+      subtitle = subtitle,
+      color = "Rural/Urban"
+    ) +
+    ggplot2::scale_color_brewer(palette = "Accent") +
+    ggplot2::theme_void() +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(hjust = 0.5),
+      plot.subtitle = ggplot2::element_text(hjust = 0.5)
+    )
+
+  # Return
+  return(plot_map)
+}
